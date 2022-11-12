8a3fa0a41b34eacc18ef06d01bbcce5ebaf93bc3
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-12 11:33:59 +0100

    feat: finish the tutorial
    It was full of mistakes, but whatever.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,63 @@
 module.exports.onRpcRequest = async ({ origin, request }) => {
+  let state = await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  });
+
+  if (!state) {
+    state = { book: [] };
+    // initialize state if empty and set default data
+    await wallet.request({
+      method: 'snap_manageState',
+      params: ['update', state],
+    });
+  }
+
   switch (request.method) {
+    case 'storeAddress':
+      state.book.push({
+        name: request.params.nameToStore,
+        address: request.params.addressToStore,
+      });
+
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', state],
+      });
+      return true;
+    // return wallet.request({
+    //   method: 'snap_confirm',
+    //   params: [
+    //     {
+    //       prompt: `Hello, ${origin}!`,
+    //       description: 'The address has been saved to your address book',
+    //       textAreaContent:
+    //         `Name: ${request.params.nameToStore}\n` +
+    //         `Address: ${request.params.addressToStore}\n` +
+    //         `Addresses in book: ${state.book.length}`,
+    //     },
+    //   ],
+    // });
+    case 'retrieveAddresses':
+      return state.book;
     case 'hello':
+      // eslint-disable-next-line no-case-declarations
+      // const addressBook = state.book
+      //   .map(function (item) {
+      //     return `${item.name}: ${item.address}`;
+      //   })
+      //   .join('\n');
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
             prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+            description: 'Address book:',
+            textAreaContent: state.book
+              .map(function (item) {
+                return `${item.name}: ${item.address}`;
+              })
+              .join('\n'),
           },
         ],
       });
